6abfe24f8c48cbafd5d13693e0b124c3be041b61
Guoyang Guo <guoyang@Guoyangs-mbp.local>
2022-09-25 16:34:04 +0800

    add base.go

---
+++ base/base.go
@@ -0,0 +1,13 @@
+package base
+
+// Remove
+func RemoveItem(slc []string, item string) []string {
+	i := 0
+	for _, v := range slc {
+		if v != item {
+			slc[i] = v
+			i++
+		}
+	}
+	return slc
+}
